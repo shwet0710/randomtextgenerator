14e5d074cc14bc1689bcfe482c70367bc80601d6
shwet0710 <shwetshukla0710@gmail.com>
2022-12-28 15:29:57 +0530

    added codes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = { 
+    advice: ' ',
+  }
+  componentDidMount() {
+    this.fetching();
+  }
+  fetching = () => {
+    axios.get('https://api.adviceslip.com/advice')
+         .then((res) => {
+          const { advice } = res.data.slip;
+          this.setState({advice});
+         })
+         .catch((err) => {
+          console.log(err);
+         });
+  }
+  render() {
+    return (
+      <div className='app'>
+        <div className='card'>
+          <h1 className='title'>{this.state.advice}</h1>
+          <button className="button" onClick={this.fetching}>
+            <span>GIVE ME ADVICE!</span>
+          </button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
